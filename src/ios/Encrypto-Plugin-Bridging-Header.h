85fc4a21fba27ae6fce7454ecd9d4b3d1de23d28
gapaus <a.sabirzyanov@encryptotel.com>
2019-01-17 02:50:21 +0300

    chat

--- src/ios/Encrypto-Plugin-Bridging-Header.h
+++ src/ios/Encrypto-Plugin-Bridging-Header.h
@@ -1,1 +1,9 @@
-#import <CommonCrypto/CommonCrypto.h>
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwCrypt.
+FOUNDATION_EXPORT double SwCryptVersionNumber;
+
+//! Project version string for SwCrypt.
+FOUNDATION_EXPORT const unsigned char SwCryptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwCrypt/PublicHeader.h>
