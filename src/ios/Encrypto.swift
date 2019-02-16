a9752cc8a3de255542d4363c480840d108f0b14c
gapaus <a.sabirzyanov@encryptotel.com>
2019-02-16 13:01:40 +0300

    chat

--- src/ios/Encrypto.swift
+++ src/ios/Encrypto.swift
@@ -15,22 +15,24 @@ import SwCrypt
 
         let messageArr = Data(base64Encoded: message)!;
         let tag = Data(count: 0);
+        var decrypted = "";
 
-        let encSessionKey = messageArr[0..<256]
-        let sessionKey = try! CC.RSA.decrypt(encSessionKey, derKey: privateKeyDER, tag: tag, padding: .oaep, digest: .sha1)
+        if messageArr.count > 0 {
+            let encSessionKey = messageArr[0..<256]
+            let sessionKey = try! CC.RSA.decrypt(encSessionKey, derKey: privateKeyDER, tag: tag, padding: .oaep, digest: .sha1)
 
-        let encIv = messageArr[256..<512]
-        let iv = try! CC.RSA.decrypt(encIv, derKey: privateKeyDER, tag: tag, padding: .oaep, digest: .sha1)
+            let encIv = messageArr[256..<512]
+            let iv = try! CC.RSA.decrypt(encIv, derKey: privateKeyDER, tag: tag, padding: .oaep, digest: .sha1)
 
-        let encData = messageArr[512..<messageArr.count]
-        let decodedData = try! CC.crypt(.decrypt, blockMode: .cbc, algorithm: .aes, padding: .pkcs7Padding, data: encData, key: sessionKey.0, iv: iv.0)
-        let decrypted = String(decoding: decodedData, as: UTF8.self)
+            let encData = messageArr[512..<messageArr.count]
+            let decodedData = try! CC.crypt(.decrypt, blockMode: .cbc, algorithm: .aes, padding: .pkcs7Padding, data: encData, key: sessionKey.0, iv: iv.0)
+            decrypted = String(decoding: decodedData, as: UTF8.self)
 //        print(decodedData.map {
 //            String(format: "%02hhx", $0)
 //        }.joined());
 
+        }
         pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: decrypted)
-
         self.commandDelegate!.send(
                 pluginResult,
                 callbackId: command.callbackId
